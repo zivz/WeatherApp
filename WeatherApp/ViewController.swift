22fbe32ce62bc440495a4078613c1ece87a08f74
Zalzstein, Ziv <ziv.zalzstein@intl.att.com>
2020-02-03 11:30:28 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Zalzstein, Ziv on 03/02/2020.
+//  Copyright © 2020 Zalzstein, Ziv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
